276dd74bb078402c1f6e0bb9c96ebd77c27a4cf6
Sn0wC0de <dmitrijbeleckij@gmail.com>
2021-12-15 20:38:57 +0100

    final

--- script2.js
+++ script2.js
@@ -14,19 +14,7 @@ function forLandingFraze() {
 };
 
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
 
-  if (prevScrollpos > currentScrollPos) {
-    console.log(0)
-    document.getElementById("header").style.top = "0";
-  } else {
-    console.log(50)
-    document.getElementById("header").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
 // on load
 
 forLandingFraze()

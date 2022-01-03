af3fad48d6231dfeaa964aaff2ab4585e53094fb
Sn0wC0de <dmitrijbeleckij@gmail.com>
2022-01-03 20:29:11 +0100

    new style for mobile view

--- script2.js
+++ script2.js
@@ -1,14 +1,46 @@
 const landingText = document.getElementById("landing-text");
 
+// bollean to discover name in forLandingFraze()
+let specialLeter = false;
+
 function forLandingFraze() {
+    // split fraze on dots
     let sentenceList = landingFraze.split(".");
+    
+    
+  
     sentenceList.forEach((element,i) => {
-        let div = document.createElement('div');
-        element = element + '!';
-        if (element.length > 1) {
-        let div = document.createElement('div');
-        div.textContent = element;
-          landingText.appendChild(div) }
+        // put ! in front of the name so I can find it later
+        if(element.includes("Dmitrij")){
+            element = element.replace("Dmitrij" , "!Dmitrij!")
+            let newElList = element.split( '!', 2);
+            newElList.forEach((el)=> {
+                if(el.includes("Dmitrij")) {
+                    el = el+'!';
+                    let div = document.createElement('div');
+                    div.classList.add("name")
+                    console.log(el)
+                    div.textContent = el
+                    landingText.appendChild(div)
+                } else {
+                    let div = document.createElement('div');
+                    div.textContent = el;
+                    landingText.appendChild(div)
+                }
+                
+            })
+            
+          } else {
+            let div = document.createElement('div');
+            element = element + '!';
+            if (element.length > 1) {
+            let div = document.createElement('div');
+            div.textContent = element;
+            landingText.appendChild(div) }
+          }
+
+
+        
       })   
 };
 
